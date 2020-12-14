99fd96eca1faa907fa3b7233a3fcace9c9e5c701
jhonatan torres <jtorresf@unal.edu.co>
2020-12-14 07:13:13 -0500

    1.0.0 backend intregration

--- src/App.js
+++ src/App.js
@@ -3,299 +3,264 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import AceEditor from 'react-ace';
-import 'brace/mode/java'
-import 'brace/theme/monokai'
+import 'brace/mode/java';
+import 'brace/theme/monokai';
 
-import "./estilos/palette.css"
-import "./App.css"
+import './estilos/palette.css';
+import './App.css';
 
-import logo2 from "./logo2.png"
-import exito2 from "./exito2.png"
+import logo2 from './logo2.png';
+import exito2 from './exito2.png';
 
-import prueba from "./prueja.json"
+import prueba from './prueja.json';
 
-
-import Parameters from "./componentes/parameters";
-import Sugerencia from "./componentes/sugerencia";
-
-
+import Parameters from './componentes/parameters';
+import Sugerencia from './componentes/sugerencia';
 
 class App extends Component {
+  state = {
+    minLineComment: 0,
+    Complejidad: 0,
+    maxCaracterByFuntion: 0,
+    maxLinesByFuntion: 0,
+    maxLineComment: 0,
+    maxClass: 0,
+    maxFuntionByClass: 0,
+    sugerencias: [],
+    textInput: '',
+    error: false,
+    image: false,
+  };
 
-	state={
+  setText = (param) => {
+    this.setState({
+      textInput: param,
+    });
+  };
+  seterror = (param) => {
+    this.setState({
+      error: param,
+    });
+  };
+  setminLineComment = (param) => {
+    this.setState({
+      minLineComment: param,
+    });
+  };
 
-        minLineComment : 0,
-        Complejidad : 0,
-        maxCaracterByFuntion : 0,
-        maxLinesByFuntion: 0,
-        maxLineComment: 0,
-        maxClass: 0,
-		maxFuntionByClass: 0,
-		sugerencias: [],
-		textInput:"",
-		error: false,
-		image: false,
+  setComplejidad = (param) => {
+    param = parseInt(param);
+    this.setState({
+      Complejidad: param,
+    });
+  };
 
-    }
-	
-	setText = (param) =>{
-        this.setState({
-            textInput: param
-        })
-             
-    }
-	seterror = (param) =>{
-        this.setState({
-            error: param
-        })
-             
-    }
-    setminLineComment = (param) =>{
-        this.setState({
-            minLineComment: param
-        })
-             
-    }
+  setmaxCaracterByFuntion = (param) => {
+    param = parseInt(param);
+    this.setState({
+      maxCaracterByFuntion: param,
+    });
+  };
 
-    setComplejidad = (param) =>{
+  setmaxLinesByFuntion = (param) => {
+    param = parseInt(param);
+    this.setState({
+      maxLinesByFuntion: param,
+    });
+  };
 
-        param = parseInt(param)
-        this.setState({
-            Complejidad: param
-        })
-             
-    }
+  setmaxLineComment = (param) => {
+    param = parseInt(param);
+    this.setState({
+      maxLineComment: param,
+    });
+  };
 
-    setmaxCaracterByFuntion = (param) =>{
-        param = parseInt(param)
-        this.setState({
-            maxCaracterByFuntion: param
-        })
-             
-    }
+  setmaxClass = (param) => {
+    param = parseInt(param);
+    this.setState({
+      maxClass: param,
+    });
+  };
 
-    setmaxLinesByFuntion = (param) =>{
-        param = parseInt(param)
-        this.setState({
-            maxLinesByFuntion: param
-        })
-             
-    }
+  setmaxFuntionByClass = (param) => {
+    param = parseInt(param);
+    this.setState({
+      maxFuntionByClass: param,
+    });
+  };
+  setSugerencias = (param) => {
+    this.setState({
+      sugerencias: param,
+    });
+    //console.log(this.state)
+  };
 
-    setmaxLineComment = (param) =>{
-        param = parseInt(param)
-        this.setState({
-            maxLineComment: param
-        })
-             
-    }
+  setImage = (param) => {
+    this.setState({
+      image: param,
+    });
+    //console.log(this.state)
+  };
 
-    setmaxClass = (param) =>{
-        param = parseInt(param)
-        this.setState({
-            maxClass: param
-        })
-             
-    }
+  postRequest = () => {
+    const deptsURL = process.env.REACT_APP_API_URL;
+    var vAttributes = {
+      complexity: this.state.Complejidad,
+      minCommentedLinesCount: this.state.minLineComment,
+      maxNameLength: this.state.maxCaracterByFuntion,
+      maxLineCountByFunction: this.state.maxLinesByFuntion,
+      maxCommentedLinesCount: this.state.maxLineComment,
+      maxClassesByFile: this.state.maxClass,
+      maxFunctionCountByClass: this.state.maxFuntionByClass,
+      text: this.state.textInput,
+    };
 
-    setmaxFuntionByClass = (param) =>{
-        param = parseInt(param)
-        this.setState({
-            maxFuntionByClass: param
-        })
-             
-    }
-	setSugerencias = (param) => {
-        this.setState({
-            sugerencias: param
-		})
-		//console.log(this.state)
+    const ops = {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      data: JSON.stringify(vAttributes),
+      url: deptsURL,
+    };
 
-	}
+    axios(ops)
+      .then((res) => {
+        console.log('res.data');
+        console.log(res);
+        if (res.data.letngth == 0) {
+          this.setImage(true); //muestra imagen
+        } else {
+          this.setImage(false);
+        }
+        this.setSugerencias(res.data);
+      })
+      .catch((error) => {
+        alert('There was an error ' + error.message);
+        console.log('error.data');
+        console.log(error);
+      });
+  };
 
+  correctText = (prev, correct) => {
+    let text = this.state.textInput;
+    let newtext = text.replaceAll(prev, correct);
+    this.aceEditorRef.editor.setValue(newtext);
+    this.setState({
+      textInput: newtext,
+    });
+  };
 
-	setImage = (param) => {
-		this.setState({
-            image: param
-		})
-		//console.log(this.state)
-	}
-	
-	postRequest=()=>{
+  render() {
+    let table = [];
 
-		const deptsURL = "https://code-style-analyzer.herokuapp.com/java/code-analysis/";
-		var vAttributes = {};
-		vAttributes = {
-			complexity: this.state.Complejidad,
-			minCommentedLinesCount: this.state.minLenghtLineComment,
-			maxNameLength: this.state.maxCaracterByFuntion,
-			maxLineCountByFunction: this.state.maxLinesByFuntion,
-			maxCommentedLinesCount: this.state.maxLenghtLineComment,
-			maxClassesByFile: this.state.maxClassByFile,
-			maxFunctionCountByClass: this.state.maxNumFuntionByClass,
-			text: this.state.textInput
-		};
+    this.state.sugerencias.map((data, index) => {
+      //console.log(data)
+      table.push(
+        <Sugerencia
+          key={index}
+          error={data.errorMessage}
+          sugerencia={data.suggestion}
+          linea={data.line}
+          col={data.column}
+          hasCorr={data.hasCorrection}
+          prevCode={data.previousCode}
+          correctedCode={data.correctedCode}
+          correctText={this.correctText}
+        />
+      );
 
-		const ops = {
-			method: 'POST',
-			headers: { 'content-type': 'application/json' },
-			data: JSON.stringify(vAttributes) ,
-			url: deptsURL
-		};
+      return null;
+    });
+    return (
+      <div>
+        <header>
+          <nav className='navbar navbar-light'>
+            <img
+              alt='Brand'
+              src={logo2}
+              className='navbar-brand mx-auto d-block img-fluid'
+              width='500px'
+            ></img>
+          </nav>
+        </header>
+        <section>
+          <div className='container columna'>
+            <h2 className='cont font-change'>Parámetros personalizables</h2>
+            <Parameters
+              setminLineComment={this.setminLineComment}
+              setComplejidad={this.setComplejidad}
+              setmaxCaracterByFuntion={this.setmaxCaracterByFuntion}
+              setmaxLinesByFuntion={this.setmaxLinesByFuntion}
+              setmaxLineComment={this.setmaxLineComment}
+              setmaxClass={this.setmaxClass}
+              setmaxFuntionByClass={this.setmaxFuntionByClass}
+            ></Parameters>
 
-		axios( ops).then((res) => {
-					console.log("res.data")
-					console.log(res)
-			/* Correcto descomentar al emparejar con el back
-				if (res.data.leght()>0){
-					this.setImage(false) //oculta imagen
-					this.setSugerencias(prueba)
-				}else{
-					this.setImage(true) //muestra imagen
-					this.setSugerencias([])
-				}
-			*/
-			this.setImage(true) //muestra imagen
-			this.setSugerencias([])
-				
-			}
-			).catch((error) =>{
-					//Quitar al unir con el back
-					this.setImage(false) //oculta imagen
-					this.setSugerencias(prueba)
-					console.log("error.data")
-					console.log(error)
-					// this.setImage(true)
-				
-			});
-		
+            <div className='row row justify-content-between'>
+              <div className='col-lg-6'>
+                <div className='text-button separe-items'>
+                  <div className=''>
+                    <h2 className='font-change'>Codigo</h2>
+                  </div>
+                  <div className=''>
+                    <button
+                      className='btn button-color'
+                      onClick={() => {
+                        // this.setSugerencias(prueba) //COMENTAR
+                        this.postRequest();
+                      }}
+                    >
+                      Analizar
+                    </button>
+                  </div>
+                </div>
 
-	}
+                <div className='input-group'>
+                  <div className='input-group-prepend'>
+                    <span className='input-group-text'></span>
+                  </div>
 
-	correctText=(prev,correct)=>{
-		
-		let text = this.state.textInput
-		//console.log(text)
-		let newtext = text.replaceAll(prev, correct)
-		//console.log(newtext)
-		this.refs.aceEditor.editor.setValue(newtext)
-		this.setState({
-			textInput: newtext
-		})
+                  <AceEditor
+                    mode='javascript'
+                    theme='monokai'
+                    ref={(c) => (this.aceEditorRef = c)}
+                    onChange={(code) => {
+                      //console.log(code)
+                      this.setText(code);
+                    }}
+                  />
+                </div>
+              </div>
 
-	}
+              <div className='col-lg-5 contfin '>
+                <h2 className='font-change'>Sugerencias</h2>
 
-
-	render(){
-
-		let table = [];
-
-        this.state.sugerencias.map(
-            (data, index) => {
-				//console.log(data)
-                    table.push(
-						
-                        <Sugerencia
-							key={index}
-							error = {data.errorMessage}
-							sugerencia =  {data.suggestion}
-							linea =  {data.line}
-							col =  {data.column}  	
-							hasCorr = {data.hasCorrection}
-							prevCode = {data.previousCode}
-							correctedCode = {data.correctedCode}
-							correctText ={this.correctText}
-						/>)
-					
-					return null;
-					}
-				);
-  return (
-    <div>
-      <header>  
-		
-        <nav className="navbar navbar-light">
-		  <img alt="Brand" src={logo2} className="navbar-brand mx-auto d-block img-fluid" width="500px"></img>
-          
-        </nav>    
-      </header>
-      <body>
-	  	<div className="container columna">
-			<h2 className="cont font-change">Parámetros personalizables</h2>
-			<Parameters 
-				setminLineComment = {this.setminLineComment}
-				setComplejidad = {this.setComplejidad}			
-				setmaxCaracterByFuntion = {this.setmaxCaracterByFuntion}			
-				setmaxLinesByFuntion = {this.setmaxLinesByFuntion}			
-				setmaxLineComment = {this.setmaxLineComment}			
-				setmaxClass = {this.setmaxClass}			
-				setmaxFuntionByClass = {this.setmaxFuntionByClass}	
-			>
-			</Parameters>
-
-        
-          <div className = "row row justify-content-between">
-
-            <div className = "col-lg-6">
-				<div className = "text-button separe-items">
-					<div className = "">
-						<h2 className = "font-change">Codigo</h2>
-					</div>
-					<div className = "">
-						<button className= "btn button-color"
-							onClick={() => { 
-									// this.setSugerencias(prueba) //COMENTAR
-									this.postRequest()
-								}}
-							>Analizar
-						</button>
-					</div>
-				</div>
-              
-			  	<div className="input-group">
-					<div className="input-group-prepend">
-						<span className="input-group-text"></span>
-					</div>
-
-					<AceEditor  
-						mode="javascript" 
-						theme="monokai"
-						ref="aceEditor"
-						onChange={(code)=>{
-							//console.log(code)
-							this.setText(code)
-						}}
-					/>
-		
-				</div>
+                {this.state.image ? (
+                  <img
+                    alt='exito'
+                    src={exito2}
+                    className='mx-auto d-block img-fluid'
+                    width='340px'
+                  ></img>
+                ) : (
+                  <div
+                    className='row row-cols-1 scroll'
+                    style={{ paddingRight: '15px' }}
+                  >
+                    {table}
+                  </div>
+                )}
+              </div>
             </div>
-
-            <div className = "col-lg-5 contfin ">
-              	<h2 className = "font-change">Sugerencias</h2>
-
-				  {this.state.image ? (
-					<img alt="exito" src={exito2} className="mx-auto d-block img-fluid" width="340px"></img>
-					) : (
-						<div className="row row-cols-1 scroll"
-				  			style={{paddingRight:"15px"}}>			  	
-                		{table}
-						</div>	
-					)}
-
-            </div>
-
           </div>
-        </div>
-        
-      </body>
-		<footer className="page-footer font-small blue footer-space">
-			<div className="footer-copyright text-center py-3">
-				<p> Lenguajes de Programación 2020-2</p>
-			</div>
-		</footer>
-    </div>
-	
-  );
-}
+        </section>
+        <footer className='page-footer font-small blue footer-space'>
+          <div className='footer-copyright text-center py-3'>
+            <p> Lenguajes de Programación 2020-2</p>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 export default App;
--- src/componentes/parameters.js
+++ src/componentes/parameters.js
@@ -1,131 +1,164 @@
 import React from 'react';
 import { Component } from 'react';
-import "../estilos/general.css"
+import '../estilos/general.css';
 
 class Parameters extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='row justify-content-around align-items-center'>
+          <div className='columna col-lg-2 col-md-5 col-sm-12'>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
+                </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='LinasF'
+                  aria-describedby='basic-addon3'
+                  placeholder='Lineas por funcion'
+                  aria-label='LinasF'
+                  onChange={(e) => {
+                    this.props.setmaxLinesByFuntion(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='columna col-lg-3 col-md-5 col-sm-12'>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
+                </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='Ambitos'
+                  aria-describedby='basic-addon3'
+                  placeholder='Ambitos anidados'
+                  aria-label='Ambitos'
+                  onChange={(e) => {
+                    this.props.setComplejidad(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
+                </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='Caracteres'
+                  aria-describedby='basic-addon3'
+                  placeholder='Caracteres por nombre'
+                  aria-label='Caracteres'
+                  onChange={(e) => {
+                    this.props.setmaxCaracterByFuntion(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='columna col-lg-3 col-md-5 col-sm-12'>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
+                </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='MinLineasC'
+                  aria-describedby='basic-addon3'
+                  placeholder='Min lineas comentadas'
+                  aria-label='MinLineasC'
+                  onChange={(e) => {
+                    this.props.setminLineComment(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
+                </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='MaxLineasC'
+                  aria-describedby='basic-addon3'
+                  placeholder='Max lineas comentadas'
+                  aria-label='MaxLineasC'
+                  onChange={(e) => {
+                    this.props.setmaxLineComment(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
 
-    render(){
-    return(
-    <div className="container">
-    <div className="row justify-content-around align-items-center">
-        <div className ="columna col-lg-2 col-md-5 col-sm-12">
-            
-            <div className="row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="LinasF" aria-describedby="basic-addon3"
-                    placeholder="Lineas por funcion" aria-label="LinasF"
-                    onChange={()=>{
-                        let text = document.getElementById("LinasF").value;
-                        this.props.setmaxLinesByFuntion(text)
-                        }} />
-                
+          <div className='columna col-lg-3 col-md-5 col-sm-12'>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
                 </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='Funciones'
+                  aria-describedby='basic-addon3'
+                  placeholder='Funciones por clase'
+                  aria-label='Funciones'
+                  onChange={(e) => {
+                    this.props.setmaxFuntionByClass(e.target.value);
+                  }}
+                />
+              </div>
             </div>
+            <div className='row'>
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text' id='basic-addon3'>
+                    #
+                  </span>
+                </div>
+                <input
+                  type='number'
+                  className='form-control'
+                  id='Clases'
+                  aria-describedby='basic-addon3'
+                  placeholder='Clases por archivo'
+                  aria-label='Clases'
+                  onChange={(e) => {
+                    this.props.setmaxClass(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className ="columna col-lg-3 col-md-5 col-sm-12">
-            
-            <div className = "row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="Ambitos" aria-describedby="basic-addon3"
-                    placeholder="Ambitos anidados" aria-label="Ambitos"
-                    
-                    onChange={()=>{
-                        let text = document.getElementById("Ambitos").value;
-                        this.props.setComplejidad(text)
-                        }}
-                        
-                        />
-                </div>
-            </div>
-            <div className = "row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="Caracteres" aria-describedby="basic-addon3"
-                    placeholder="Caracteres por nombre" aria-label="Caracteres"
-                    onChange={()=>{
-                        let text = document.getElementById("Caracteres").value;
-                        this.props.setmaxCaracterByFuntion(text)
-                        }}/>
-                </div>
-            </div>
-            
-        </div>
-        <div className ="columna col-lg-3 col-md-5 col-sm-12">
-            
-            <div className = "row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="MinLineasC" aria-describedby="basic-addon3"
-                    placeholder="Min lineas comentadas" aria-label="MinLineasC"
-                    onChange={()=>{
-                        let text = document.getElementById("MinLineasC").value;
-                        this.props.setminLineComment(text)
-                        }}/>
-                </div>
-            </div>
-            <div className = "row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="MaxLineasC" aria-describedby="basic-addon3"
-                    placeholder="Max lineas comentadas" aria-label="MaxLineasC"
-                    onChange={()=>{
-                        let text = document.getElementById("MaxLineasC").value;
-                        this.props.setmaxLineComment(text)
-                        }}/>
-                </div>
-                
-            </div>
-        </div>
-
-        <div className ="columna col-lg-3 col-md-5 col-sm-12">
-            
-            
-            <div className = "row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="Funciones" aria-describedby="basic-addon3"
-                    placeholder="Funciones por clase" aria-label="Funciones"
-                    onChange={()=>{
-                        let text = document.getElementById("Funciones").value;
-                        this.props.setmaxFuntionByClass(text)
-                        }}/>
-                </div>
-            </div>
-            <div className = "row">
-                <div className="input-group mb-3">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="basic-addon3">#</span>
-                    </div>
-                    <input type="number" className="form-control" id="Clases" aria-describedby="basic-addon3"
-                    placeholder="Clases por archivo" aria-label="Clases"
-                    onChange={()=>{
-                        let text = document.getElementById("Clases").value;
-                        this.props.setmaxClass(text)
-                        }}/>
-                </div>
-            </div>
-        
-        </div>
-       
-     </div>  
-     </div> 
-
-
+      </div>
     );
-    }
+  }
 }
 export default Parameters;
